4ad9090922126d4fe5cc767989ce3c7a4eeb1c0d
dinanuroktavia <32434972+dinanuroktavia@users.noreply.github.com>
2017-10-25 02:57:14 +1100

    jumlah box parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -9,4 +9,8 @@ int main(){
 	
 	cout << "Luas Rumah				:"<<LuasRumah<<" m2"<<endl;
 	cout <<"Luas 1 Box				:"<<Luas1Box<<" m2"<<endl;
+	
+	JumlahBoxP = LuasRumah/Luas1Box;
+	cout<<"Jumlah Box Parket		:"<<JumlahBoxP<<endl;
+	
 }
